07ca64332b13423e8bccc4b98ad505b4dc4208db
Lisa Koss <lisakoss@uw.edu>
2018-02-09 12:22:12 -0800

    Add sign up constructor

---
+++ src/SignUp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import firebase from 'firebase';
+
+class SignUp extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            value: null,
+        };
+    }
+}
