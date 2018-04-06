73fe31bd05fe4db3472a4e937fef0d2456341d35
Lisa Koss <lisakoss@uw.edu>
2018-04-06 02:18:35 -0700

    Group map cards + label number of pickups

--- src/Listing.js
+++ src/Listing.js
@@ -10,110 +10,119 @@ import ListingsForm from './ListingsForm';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 const styles = {
-    snack: {
-        textAlign: 'center',
-    },
-    progress: {
-        marginTop: '10px'
-    }
+  snack: {
+    textAlign: 'center',
+  },
+  progress: {
+    marginTop: '10px'
+  }
 }
 
 class Listing extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {error: null, 
-            isSnackBarActive: false, 
-            spinnerDisplay: false};
+  constructor(props) {
+    super(props)
+    this.state = {
+      error: null,
+      isSnackBarActive: false,
+      spinnerDisplay: false
+    };
 
-        this.submit = this.submit.bind(this);
-        this.loadApp = this.loadApp.bind(this);
-        this.handleTimeoutSnackbar = this.handleTimeoutSnackbar.bind(this);
+    this.submit = this.submit.bind(this);
+    this.loadApp = this.loadApp.bind(this);
+    this.handleTimeoutSnackbar = this.handleTimeoutSnackbar.bind(this);
+  }
+
+  // redirect
+  loadApp() {
+    this.props.history.push('/home');
+  }
+
+  //Lifecycle callback executed when the component appears on the screen.
+  componentDidMount() {
+    // Add a listener and callback for authentication events
+    this.unregister = firebase.auth().onAuthStateChanged(user => {
+      /*if(user) { //redirect to board once user is signed up
+          this.setState({userId:user.uid});
+          this.loadApp();
+      }
+      else{
+          this.setState({userId: null}); //null out the saved state
+      }*/
+    });
+  }
+
+  //when the component is unmounted, unregister using the saved function
+  componentWillUnmount() {
+    if (this.unregister) { //if have a function to unregister with
+      this.unregister(); //call that function!
+    }
+  }
+
+  submit(location, boxes, expirationDate, weight, tags) {
+    let thisComponent = this;
+    thisComponent.setState({ spinnerDisplay: true }); //show loading spinner while user is being signed up
+    thisComponent.setState({ isSnackbarActive: true }); //show snackbar, where spinner is located, while user is being signed up
+
+    let listingsRef = firebase.database().ref('listings');
+    let newListing = {
+      location: location,
+      boxes: boxes,
+      expirationDate: String(expirationDate),
+      weight: weight,
+      tags: tags,
+      time: firebase.database.ServerValue.TIMESTAMP,
+      userId: firebase.auth().currentUser.uid
     }
 
-    // redirect
-    loadApp() {
-        this.props.history.push('/home');
+    let listing = listingsRef.push(newListing); // upload msg to database
+    let listingId = listing.key;
+
+    /* Add listing to user's id */
+    let currUser = firebase.auth().currentUser.uid;
+    let usersRef = firebase.database().ref('users/' + currUser + '/listings');
+    let newUserListing = {
+      listingId: listingId,
+    }
+    usersRef.push(newUserListing);
+
+    /* Add listing to appropriate farmer's market */
+    let marketsRef = firebase.database().ref(`markets/${location.split(",")[0]}`);
+    let newMarketListing = {
+      listingId: listingId,
+    }
+    marketsRef.push(newMarketListing);
+  }
+
+  //when an error snackbar appears, it will eventually time out and disppear
+  handleTimeoutSnackbar() {
+    this.setState({ isSnackbarActive: false });
+  }
+
+  render() {
+    let content = <div><ListingsForm submitCallback={this.submit} /></div>; //what main content to show
+    let snackbarContent = null; //what snackbar content to show
+
+    if (this.state.userId) { //if logged in
+      content = (<div><ListingsForm submitCallback={this.submit} /></div>);
     }
 
-    //Lifecycle callback executed when the component appears on the screen.
-    componentDidMount() {
-        // Add a listener and callback for authentication events
-        this.unregister = firebase.auth().onAuthStateChanged(user => {
-            /*if(user) { //redirect to board once user is signed up
-                this.setState({userId:user.uid});
-                this.loadApp();
-            }
-            else{
-                this.setState({userId: null}); //null out the saved state
-            }*/
-        });
+    if (this.state.spinnerDisplay) { //show spinner when loading
+      snackbarContent = <CircularProgress style={styles.progress} />;
+    } else if (this.state.error !== undefined) { //otherwise show error message
+      snackbarContent = this.state.error;
     }
 
-    //when the component is unmounted, unregister using the saved function
-    componentWillUnmount() {
-        if(this.unregister){ //if have a function to unregister with
-        this.unregister(); //call that function!
-        }
-    }
+    return (
+      <div>
+        <main role="article">
+          {content}
+        </main>
+        <div role="region">
 
-    submit(location, boxes, expirationDate, weight, tags) {
-        let thisComponent = this;
-        thisComponent.setState({spinnerDisplay: true}); //show loading spinner while user is being signed up
-        thisComponent.setState({isSnackbarActive: true}); //show snackbar, where spinner is located, while user is being signed up
-   
-        let listingsRef = firebase.database().ref('listings');
-        let newListing = {
-            location: location,
-            boxes: boxes,
-            expirationDate: String(expirationDate),
-            weight: weight,
-            tags: tags,
-            time: firebase.database.ServerValue.TIMESTAMP,
-            userId: firebase.auth().currentUser.uid
-        }
-        
-        let listing = listingsRef.push(newListing); // upload msg to database
-        let listingId = listing.key;
-
-        /* Add listing to user's id */
-        let currUser = firebase.auth().currentUser.uid;
-        let usersRef = firebase.database().ref('users/' + currUser + '/listings');
-        let newUserListing = {
-            listingId: listingId,
-        }
-        usersRef.push(newUserListing);
-    }
-
-    //when an error snackbar appears, it will eventually time out and disppear
-    handleTimeoutSnackbar() {
-        this.setState({ isSnackbarActive: false });
-    }
-
-    render() {
-        let content = <div><ListingsForm submitCallback={this.submit} /></div>; //what main content to show
-        let snackbarContent = null; //what snackbar content to show
-
-        if(this.state.userId) { //if logged in
-            content = (<div><ListingsForm submitCallback={this.submit} /></div>);
-        } 
-
-        if(this.state.spinnerDisplay) { //show spinner when loading
-            snackbarContent = <CircularProgress style={styles.progress}/>;
-          } else if(this.state.error !== undefined) { //otherwise show error message
-            snackbarContent = this.state.error;
-        }
-
-        return(
-            <div>
-                <main role="article">
-                    {content}
-                </main>
-                <div role="region">
-
-                </div>
-            </div>
-        );
-    }
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Listing;
--- src/MapContainer.js
+++ src/MapContainer.js
@@ -12,156 +12,189 @@ import MapCards from './MapCards';
 import GoogleApiComponent from 'google-maps-react/dist/GoogleApiComponent';
 
 export class Container extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            showingInfoWindow: false,
-            activeMarker: {},
-            selectedPlace: {},
-            markers: [],
-            listings: [],
-            distances: [],
-            currentLocation: {
-                lat: 0,
-                lng: 0
-            },
-            mapCards: []
-        }
-
-        this.onMarkerClick = this.onMarkerClick.bind(this);
-        this.onMapClick = this.onMapClick.bind(this);
-        this.onInfoWindowClose = this.onInfoWindowClose.bind(this);
-        this.handleLocation = this.handleLocation.bind(this);
+    this.state = {
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: {},
+      markers: [],
+      listings: [],
+      markets: [],
+      distances: [],
+      currentLocation: {
+        lat: 0,
+        lng: 0
+      },
+      mapCards: []
     }
-        
-    //Lifecycle callback executed when the component appears on the screen.
-    componentDidMount() {
-        /* Add a listener for changes to the listings object, and save in the state. */
-        var listingsRef = firebase.database().ref('listings');
-        listingsRef.on('value', (snapshot) => {
-            var listingArray = []; 
-            snapshot.forEach(function(child){
-            var listing = child.val();
-                    listing.key = child.key; 
-                    listingArray.push(listing); 
+
+    this.onMarkerClick = this.onMarkerClick.bind(this);
+    this.onMapClick = this.onMapClick.bind(this);
+    this.onInfoWindowClose = this.onInfoWindowClose.bind(this);
+    this.handleLocation = this.handleLocation.bind(this);
+  }
+
+  //Lifecycle callback executed when the component appears on the screen.
+  componentDidMount() {
+    /* Add a listener for changes to the listings object, and save in the state. */
+    var listingsRef = firebase.database().ref('listings');
+    listingsRef.on('value', (snapshot) => {
+      var listingArray = [];
+      snapshot.forEach(function (child) {
+        var listing = child.val();
+        listing.key = child.key;
+        listingArray.push(listing);
+      });
+      //listingArray.sort((a,b) => b.time - a.time); //reverse order
+      this.setState({ listings: listingArray });
+      //fetch(`https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=Washington,DC&destinations=New+York+City,NY&key=AIzaSyBLkew0nfQHAXvEc4H9rVgGCT5wYVw19uE`)
+    });
+  }
+
+  /* When component will be removed. */
+  componentWillUnmount() {
+    //unregister listeners
+    firebase.database().ref('listings').off();
+  }
+
+  handleLocation = (locationVal) => {
+    var marketsRef = firebase.database().ref(`markets`);
+    marketsRef.on('value', (snapshot) => {
+      var marketsArray = [];
+      snapshot.forEach(function (child) {
+        var marketListing = child.val();
+        marketListing.key = child.key;
+        marketsArray.push(marketListing);
+      });
+
+      marketsArray.map((market) => {
+        let marketKeys = Object.keys(market);
+
+        // take all keys except "key", which is just the unique id of the db obj
+        for (let i = 0; i < marketKeys.length - 1; i++) {
+          let currentMarkets = this.state.markets;
+          if (!currentMarkets.includes(market.key)) {
+            currentMarkets.push(market.key);
+            this.setState({ markets: currentMarkets });
+          }
+
+          var marketListingsRef = firebase.database().ref(`markets/${market.key}/${marketKeys[i]}`);
+          marketListingsRef.on('value', (snapshot) => {
+            var marketListingsArray = [];
+            snapshot.forEach(function (child) {
+              var marketListing = child.val();
+              marketListingsArray.push(marketListing);
             });
-            //listingArray.sort((a,b) => b.time - a.time); //reverse order
-            this.setState({listings: listingArray});
-            //fetch(`https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=Washington,DC&destinations=New+York+City,NY&key=AIzaSyBLkew0nfQHAXvEc4H9rVgGCT5wYVw19uE`)
-        });
-    }
-    
-    /* When component will be removed. */
-    componentWillUnmount() {
-        //unregister listeners
-        firebase.database().ref('listings').off();
-    }
 
-    handleLocation = (locationVal) => {
-        /* Create a list of <MapCard /> objects. */
-        var mapCards = this.state.listings.map((listing) => {
-            return (
-                <MapCards 
-                    location={locationVal}
-                    title = {listing.location}
-                    google = {this.props.google}
+            if ((i + 2) == marketKeys.length) {
+              let currentMapCards = this.state.mapCards;
+              currentMapCards.push(
+                <MapCards
+                  location={locationVal}
+                  title={market.key}
+                  count={marketKeys.length - 1}
+                  google={this.props.google}
                 />
-            );
-        })
-
-        this.setState({mapCards: mapCards})
-    }
-
-    getInitialState() {
-        return {
-          showingInfoWindow: false,
-          activeMarker: {},
-          selectedPlace: {}
+              )
+              this.setState({ mapCards: currentMapCards })
+            }
+          })
         }
+      })
+    });
+  }
+
+  getInitialState() {
+    return {
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: {}
     }
+  }
 
-    onMarkerClick(props, marker, e) { 
-       this.setState({
-          selectedPlace: props,
-          activeMarker: marker,
-          showingInfoWindow: true
-        });
+  onMarkerClick(props, marker, e) {
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+  }
+
+  onMapClick() {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
     }
+  }
 
-    onMapClick() {
-        if (this.state.showingInfoWindow) {
-            this.setState({
-                showingInfoWindow: false,
-                activeMarker: null
-            });
-        }
+  onInfoWindowClose() {
+    this.setState({
+      showingInfoWindow: false,
+      activeMarker: null
+    })
+  }
+
+  render() {
+
+    /*// don't show if don't have user data yet
+    if(!this.state.users){
+        return null;
+    }*/
+
+
+    /* Create a list of <Marker /> objects. */
+    var markers = this.state.listings.map((listing) => {
+      let name = listing.location.split(",")
+      let pos = { lat: parseFloat(name[1]), lng: parseFloat(name[2]) }
+      return (
+        <Marker onClick={this.onMarkerClick}
+          name={name[0]}
+          position={pos} />
+      );
+    })
+
+    const style = {
+      width: '65%',
+      height: '100vh',
+      position: 'absolute',
+      display: 'inline-block'
     }
+    const pos = { lat: 47.7204208, lng: -122.2885376 } // where location marker goes
 
-    onInfoWindowClose() {
-        this.setState({
-          showingInfoWindow: false,
-          activeMarker: null
-        })
-    }
+    return (
+      <div className="container">
+        <div className="map-info">
+          {this.state.mapCards}
+        </div>
 
-    render() {
+        <div style={style}>
+          <Map google={this.props.google}
+            onClick={this.onMapClick}
+            onLocationChange={this.handleLocation}>
+            <Marker onClick={this.onMarkerClick}
+              name={'Current Location'} />
+            <Marker onClick={this.onMarkerClick}
+              name={'Dolores park'}
+              position={pos} />
+            {markers}
 
-        /*// don't show if don't have user data yet
-        if(!this.state.users){
-            return null;
-        }*/
-
-
-        /* Create a list of <Marker /> objects. */
-        var markers = this.state.listings.map((listing) => {
-            let name = listing.location.split(",")
-            let pos = {lat: parseFloat(name[1]), lng: parseFloat(name[2])}
-            return (
-                <Marker onClick={this.onMarkerClick}
-                name={name[0]}
-                position={pos}/>
-            );
-        })
-
-        const style = {
-            width: '65%',
-            height: '100vh',
-            position: 'absolute',
-            display: 'inline-block'
-        }
-        const pos = {lat: 47.7204208, lng: -122.2885376} // where location marker goes
-
-        return (
-            <div className="container">
-                <div className="map-info">
-                    {this.state.mapCards}
-                </div>
-                
-                <div style={style}>
-                    <Map google={this.props.google}
-                            onClick={this.onMapClick}
-                            onLocationChange={this.handleLocation}>
-                        <Marker onClick={this.onMarkerClick}
-                                name={'Current Location'}/>
-                        <Marker onClick={this.onMarkerClick}
-                                name={'Dolores park'}
-                                position={pos}/>
-                        {markers}
-
-                        <InfoWindow
-                            marker={this.state.activeMarker}
-                            visible={this.state.showingInfoWindow}
-                            onClose={this.onInfoWindowClose}>
-                            <div>
-                                <h1>{this.state.selectedPlace.name}</h1>
-                            </div>
-                        </InfoWindow>
-                    </Map>
-                </div>
-            </div>
-        );
-    }
+            <InfoWindow
+              marker={this.state.activeMarker}
+              visible={this.state.showingInfoWindow}
+              onClose={this.onInfoWindowClose}>
+              <div>
+                <h1>{this.state.selectedPlace.name}</h1>
+              </div>
+            </InfoWindow>
+          </Map>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default GoogleApiComponent({
